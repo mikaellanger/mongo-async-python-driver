32033be47134b58a0683e8cc56473f291da26bfe
Marcel Ball <me@marcelball.ca>
2011-08-23 10:48:05 -0600

    Update collection to use the new cursors

--- txmongo/collection.py
+++ txmongo/collection.py
@@ -20,30 +20,30 @@ from txmongo._pymongo import errors
 from txmongo._pymongo.son import SON
 from txmongo._pymongo.code import Code
 from txmongo._pymongo.objectid import ObjectId
-from twisted.internet.defer import Deferred
+from twisted.internet import defer
 
 
 class Collection(object):
     def __init__(self, database, collection_name):
         self._database = database
         self._collection_name = collection_name
-
+    
     def __str__(self):
         return "%s.%s" % (str(self._database), self._collection_name)
-
+    
     def __repr__(self):
         return "<mongodb Collection: %s>" % str(self)
-
+    
     def __getitem__(self, collection_name):
         return Collection(self._database,
             "%s.%s" % (self._collection_name, collection_name))
-
+    
     def __getattr__(self, collection_name):
         return self[collection_name]
-
+    
     def __call__(self, collection_name):
         return self[collection_name]
-
+    
     def _fields_list_to_dict(self, fields):
         """
         transform a list of fields from ["a", "b"] to {"a":1, "b":1}
@@ -54,10 +54,10 @@ class Collection(object):
                 raise TypeError("fields must be a list of key names")
             as_dict[field] = 1
         return as_dict
-
+    
     def _gen_index_name(self, keys):
         return u"_".join([u"%s_%s" % item for item in keys])
-
+    
     @property
     def name(self):
         """The name of this collection"""
@@ -67,7 +67,7 @@ class Collection(object):
     def full_name(self):
         '''The full name of this Collection (database_name.collection_name).'''
         return "%s.%s" % (str(self._database), self._collection_name)
-
+    
     def options(self):
         def wrapper(result):
             if result:
@@ -76,15 +76,15 @@ class Collection(object):
                     del options["create"]
                 return options
             return {}
-
+        
         d = self._database.system.namespaces.find_one({"name": str(self)})
         d.addCallback(wrapper)
         return d
-
+    
     def find(self, spec=None, skip=0, limit=0, fields=None, filter=None, slave_okay=False, _proto=None):
         if spec is None:
             spec = SON()
-
+        
         if not isinstance(spec, types.DictType):
             raise TypeError("spec must be an instance of dict")
         if not isinstance(fields, (types.ListType, types.NoneType)):
@@ -93,41 +93,38 @@ class Collection(object):
             raise TypeError("skip must be an instance of int")
         if not isinstance(limit, types.IntType):
             raise TypeError("limit must be an instance of int")
-
+        
         if fields is not None:
             if not fields:
                 fields = ["_id"]
             fields = self._fields_list_to_dict(fields)
-
+        
         if isinstance(filter, (qf.sort, qf.hint, qf.explain, qf.snapshot)):
             spec = SON(dict(query=spec))
             for k, v in filter.items():
                 spec[k] = isinstance(v, types.TupleType) and SON(v) or v
-
+        
         # send the command through a specific connection
         # this is required for the connection pool to work
         # when safe=True
         if _proto is None:
-            proto = self._database._connection(slave_okay=slave_okay)
-        else:
-            proto = _proto
-        return proto.OP_QUERY(str(self), spec, skip, limit, fields, slave_okay=slave_okay)
-
-    def find_one(self, spec=None, fields=None, slave_okay=False, _proto=None):
-        def wrapper(docs):
-            doc = docs and docs[0] or {}
-            if doc.get("err") is not None:
-                raise errors.OperationFailure(doc)
-            else:
-                return doc
-
+            _proto = self._database._connection(slave_okay=slave_okay)
+        return _proto.OP_QUERY(str(self), spec, skip, limit, fields, slave_okay=slave_okay)
+    
+    @defer.inlineCallbacks
+    def find_one(self, spec=None, fields=None, filter=None, slave_okay=False, _proto=None):
         if isinstance(spec, ObjectId):
             spec = SON(dict(_id=spec))
-
-        d = self.find(spec, limit=-1, fields=fields, slave_okay=slave_okay, _proto=_proto)
-        d.addCallback(wrapper)
-        return d
-
+        
+        cursor = yield self.find(spec, limit=-1, fields=fields, filter=filter, slave_okay=slave_okay, _proto=_proto)
+        docs = yield cursor.as_list()
+        if not docs:
+            defer.returnValue(None)
+        
+        if isinstance(docs[0], dict) and docs[0].get("err") is not None:
+            raise errors.OperationFailuer(docs[0])
+        defer.returnValue(docs[0])
+    
     def runCommand(self, command, value=1, **kwargs):
         '''
         @see: http://www.mongodb.org/display/DOCS/Commands
@@ -149,23 +146,23 @@ class Collection(object):
         cmd.update(**kwargs)
         d = self._database["$cmd"].find_one(cmd)
         return d
-
+    
     def count(self, spec=None, fields=None, slave_okay=False):
         def wrapper(result):
             return result["n"]
-
+        
         if fields is not None:
             if not fields:
                 fields = ["_id"]
             fields = self._fields_list_to_dict(fields)
-
+        
         spec = SON([("count", self._collection_name),
                     ("query", spec or SON()),
                     ("fields", fields)])
         d = self._database["$cmd"].find_one(spec, slave_okay=slave_okay)
         d.addCallback(wrapper)
         return d
-
+    
     def group(self, keys, initial, reduce, condition=None, finalize=None):
         body = {
             "ns": self._collection_name,
@@ -173,43 +170,36 @@ class Collection(object):
             "initial": initial,
             "$reduce": Code(reduce),
         }
-
+        
         if condition:
             body["cond"] = condition
         if finalize:
             body["finalize"] = Code(finalize)
-
+        
         return self._database["$cmd"].find_one({"group": body})
-
+    
     def filemd5(self, spec):
-        def wrapper(result):
-            return result.get('md5')
-
         if not isinstance(spec, ObjectId):
             raise ValueError("filemd5 expected an objectid for its on-keyword argument")
-
-        spec = SON([("filemd5", spec),
-                    ("root", self._collection_name)])
-
-        d = self._database['$cmd'].find_one(spec)
-        d.addCallback(wrapper)
-        return d
-
+        spec = SON([("filemd5", spec), ("root", self._collection_name)])
+        doc = yield self._database['$cmd'].find_one(spec)
+        defer.returnValue((doc or {}).get('md5'))
+    
     def __safe_operation(self, proto, safe=False, ids=None):
         callit = False
         if safe is True:
             d = self._database["$cmd"].find_one({"getlasterror": 1}, _proto=proto)
         else:
             callit = True
-            d = Deferred()
-
+            d = defer.Deferred()
+        
         if ids is not None:
             d.addCallback(lambda _: ids)
-
+        
         if callit is True:
             d.callback(None)
         return d
-
+    
     def insert(self, docs, safe=False):
         if isinstance(docs, types.DictType):
             ids = docs.get('_id', ObjectId())
@@ -229,7 +219,7 @@ class Collection(object):
         proto = self._database._connection()
         proto.OP_INSERT(str(self), docs)
         return self.__safe_operation(proto, safe, ids)
-
+    
     def update(self, spec, document, upsert=False, multi=False, safe=False):
         if not isinstance(spec, types.DictType):
             raise TypeError("spec must be an instance of dict")
@@ -240,37 +230,37 @@ class Collection(object):
         proto = self._database._connection()
         proto.OP_UPDATE(str(self), spec, document, upsert, multi)
         return self.__safe_operation(proto, safe)
-
+    
     def save(self, doc, safe=False):
         if not isinstance(doc, types.DictType):
             raise TypeError("cannot save objects of type %s" % type(doc))
-
+        
         objid = doc.get("_id")
         if objid:
             return self.update({"_id": objid}, doc, safe=safe, upsert=True)
         else:
             return self.insert(doc, safe=safe)
-
+    
     def remove(self, spec, safe=False):
         if isinstance(spec, ObjectId):
             spec = SON(dict(_id=spec))
         if not isinstance(spec, types.DictType):
             raise TypeError("spec must be an instance of dict, not %s" % type(spec))
-
+        
         proto = self._database._connection()
         proto.OP_DELETE(str(self), spec)
         return self.__safe_operation(proto, safe)
-
+    
     def drop(self, safe=False):
         return self.remove({}, safe)
-
+    
     def create_index(self, sort_fields, unique=False, dropDups=False):
         def wrapper(result, name):
             return name
-
+        
         if not isinstance(sort_fields, qf.sort):
             raise TypeError("sort_fields must be an instance of filter.sort")
-
+        
         name = self._gen_index_name(sort_fields["orderby"])
         index = SON(dict(
             ns=str(self),
@@ -279,11 +269,11 @@ class Collection(object):
             unique=unique,
             dropDups=dropDups
         ))
-
+        
         d = self._database.system.indexes.insert(index, safe=True)
         d.addCallback(wrapper, name)
         return d
-
+    
     def drop_index(self, index_identifier):
         if isinstance(index_identifier, types.StringTypes):
             name = index_identifier
@@ -291,52 +281,53 @@ class Collection(object):
             name = self._gen_index_name(index_identifier["orderby"])
         else:
             raise TypeError("index_identifier must be a name or instance of filter.sort")
-
+        
         cmd = SON([("deleteIndexes", self._collection_name), ("index", name)])
         return self._database["$cmd"].find_one(cmd)
-
+    
     def drop_indexes(self):
         return self.drop_index("*")
-
+    
     def index_information(self):
         def wrapper(raw):
             info = {}
             for idx in raw:
                 info[idx["name"]] = idx["key"].items()
             return info
-
+        
         d = self._database.system.indexes.find({"ns": str(self)})
         d.addCallback(wrapper)
         return d
-
+    
     def rename(self, new_name):
         cmd = SON([("renameCollection", str(self)), ("to", "%s.%s" % \
             (str(self._database), new_name))])
         return self._database("admin")["$cmd"].find_one(cmd)
-
+    
     def distinct(self, key, spec=None):
         def wrapper(result):
             if result:
                 return result.get("values")
             return {}
-
+        
         cmd = SON([("distinct", self._collection_name), ("key", key)])
         if spec:
             cmd["query"] = spec
-
+        
         d = self._database["$cmd"].find_one(cmd)
         d.addCallback(wrapper)
         return d
-
+    
     def map_reduce(self, map, reduce, full_response=False, slave_okay=False, **kwargs):
         def wrapper(result, full_response):
             if full_response:
                 return result
             return result.get("result")
-
+        
         cmd = SON([("mapreduce", self._collection_name),
                        ("map", map), ("reduce", reduce)])
         cmd.update(**kwargs)
         d = self._database["$cmd"].find_one(cmd, slave_okay=slave_okay)
         d.addCallback(wrapper, full_response)
         return d
+    
